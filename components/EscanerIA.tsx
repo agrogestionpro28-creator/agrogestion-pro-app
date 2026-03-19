b227a74732f0a2769eb5af965cbeb46f7eb6db6e
agrogestionpro28-creator <agrogestionpro2.8@gmail.com>
2026-03-19 09:25:17 -0300

    Update EscanerIA.tsx

--- components/EscanerIA.tsx
+++ components/EscanerIA.tsx
@@ -180,7 +180,7 @@ Respondé con este formato exacto:
 }`
       });
 
-      const res = await fetch("https://api.anthropic.com/v1/messages", {
+      const res = await fetch("/api/scanner", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
